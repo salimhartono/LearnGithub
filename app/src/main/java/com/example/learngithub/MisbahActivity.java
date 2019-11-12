f9946df6fed09477352a8022f30e8761f505096b
salim hartono <shalim.hartono@gmail.com>
2019-11-12 11:25:37 +0700

    #update MisbahActivity
    menambhakan komentar

--- app/src/main/java/com/example/learngithub/MisbahActivity.java
+++ app/src/main/java/com/example/learngithub/MisbahActivity.java
@@ -9,5 +9,7 @@ public class MisbahActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_misbah);
+
+        //halo misbah
     }
 }
